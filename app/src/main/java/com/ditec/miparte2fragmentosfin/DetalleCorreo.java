40e1149ae33af8c1a284aa8bdce257c4dbf97f4f
Wilson Callisaya <demarktec@gmail.com>
2020-07-17 22:22:10 -0500

    Añadiendo RecyclerView y Cardview para mostrar lista de correos usando un pojo

---
+++ app/src/main/java/com/ditec/miparte2fragmentosfin/DetalleCorreo.java
@@ -0,0 +1,14 @@
+package com.ditec.miparte2fragmentosfin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalleCorreo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_correo);
+    }
+}
